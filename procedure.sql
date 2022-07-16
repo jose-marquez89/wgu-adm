7e988c45a217d4cfb3015e6544b53d6e7768e1eb
jose-marquez89 <58154690+jose-marquez89@users.noreply.github.com>
2022-07-16 13:14:09 -0400

    Create procedure.sql

---
+++ procedure.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE PROCEDURE public.update_revenue_tables()
+ LANGUAGE sql
+AS $procedure$
+	DELETE FROM revenue_detail;
+	DELETE FROM revenue_summary;
+	
+	INSERT INTO revenue_detail
+	SELECT 
+	  s.store_id, 
+	  ct.name AS category, 
+	  date(p.payment_date) AS date_paid, 
+	  p.amount 
+	FROM 
+	  payment AS p 
+	  JOIN customer AS c on p.customer_id = c.customer_id 
+	  JOIN store AS s on c.store_id = s.store_id 
+	  JOIN rental AS r on p.rental_id = r.rental_id 
+	  JOIN inventory AS i on r.inventory_id = i.inventory_id 
+	  JOIN film_category AS fc on i.film_id = fc.film_id 
+	  JOIN category AS ct on fc.category_id = ct.category_id;
+	 
+	INSERT INTO revenue_summary 
+	SELECT 
+		store_id, 
+		category, 
+		date_paid AS business_date, 
+		SUM(amount) AS revenue
+	FROM revenue_detail
+	GROUP BY store_id, category, business_date;
+$procedure$
+;
