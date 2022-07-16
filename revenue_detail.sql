2f4d3584f7760031252035c69ba282c285f4d398
jose-marquez89 <58154690+jose-marquez89@users.noreply.github.com>
2022-07-15 23:08:02 -0500

    Update revenue_detail.sql

--- revenue_detail.sql
+++ revenue_detail.sql
@@ -5,9 +5,9 @@ SELECT
   p.amount 
 FROM 
   payment AS p 
-  JOIN customer AS con p.customer_id = c.customer_id 
+  JOIN customer AS c on p.customer_id = c.customer_id 
   JOIN store AS s on c.store_id = s.store_id 
   JOIN rental AS r on p.rental_id = r.rental_id 
   JOIN inventory AS i on r.inventory_id = i.inventory_id 
-  JOIN film_category AS fc on i.film_id - fc.film_id 
+  JOIN film_category AS fc on i.film_id = fc.film_id 
   JOIN category AS ct on fc.category_id = ct.category_id;
