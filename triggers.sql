e5b6796f6bec2acd46a816737a592464e559e753
jose-marquez89 <58154690+jose-marquez89@users.noreply.github.com>
2022-07-16 12:42:01 -0400

    Create triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE FUNCTION public.update_hold_tables()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+	DELETE FROM rental_hold_detail;
+	DELETE FROM rental_hold_summary;
+	
+	WITH hold_ledger AS (
+	  SELECT 
+	    customer_id, 
+	    rental_date, 
+	    return_date, 
+	    days_held(rental_date, return_date) AS days_held 
+	  FROM rental 
+	)
+    
+	INSERT INTO rental_hold_detail
+	SELECT 
+	  c.customer_id, 
+	  c.first_name, 
+	  c.last_name, 
+	  c.email, 
+	  DATE(l.rental_date) AS date_rented, 
+	  l.days_held 
+	FROM customer AS c 
+	JOIN hold_ledger AS l ON c.customer_id = l.customer_id;
+
+	INSERT INTO rental_hold_summary
+	SELECT 
+	  c.customer_id, 
+	  c.first_name, 
+	  c.last_name, 
+	  c.email,
+	  AVG(l.days_held) as average_days_held 
+	FROM customer AS c 
+	JOIN hold_ledger AS l ON c.customer_id = l.customer_id
+	GROUP BY c.customer_id, c.first_name, c.last_name, c.email;
+	RETURN NULL;
+END; $function$
+;
+
+-- Statement to create trigger
+CREATE TRIGGER update_rental_on_return
+AFTER UPDATE ON rental
+FOR EACH STATEMENT
+EXECUTE PROCEDURE update_hold_tables(); 
